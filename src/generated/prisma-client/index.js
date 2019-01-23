94088f284015b084a9d4e0dee86f946f1fa25c3a
Ryan Yogan <ryanyogan@gmail.com>
2019-01-22 17:40:04 -0800

    Switched to Prisma hosted services for a bit

--- src/generated/prisma-client/index.js
+++ src/generated/prisma-client/index.js
@@ -16,6 +16,6 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466/techies/dev`
+  endpoint: `https://us1.prisma.sh/ryan-yogan/techies/dev`
 });
 exports.prisma = new exports.Prisma();
